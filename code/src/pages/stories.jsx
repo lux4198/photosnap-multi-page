d7eff659e92db88b27bde83281895fb4022ee328
Lukas <lukas.nittka@posteo.de>
2022-08-16 19:12:18 +0200

    created story hero section

--- code/src/pages/stories.jsx
+++ code/src/pages/stories.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { ButtonType2 } from '../components/shared/buttons'
+import './stories.css'
 
 function Stories() {
     return (
-        <h1>Stories</h1>
+        <div class = 'story-hero-section'>
+            <div class = 'story-hero-image-mobile'/>
+            <div class = 'story-hero-text-wrap'>
+                <h4>Last month's featured story</h4>
+                <h1>Hazy full moon of Appalachia</h1>
+                <div style = {{'display' :'flex'}}>
+                    <p>March 2nd 2020</p>
+                    <p style = {{'paddingLeft' :'1em', 'color' : 'white'}}>by John Appleseed</p>
+                </div>
+                <p>  The dissected plateau area, while not actually made up of geological mountains, 
+                    is popularly called "mountains," especially in eastern Kentucky and West Virginia, 
+                    and while the ridges are not high, the terrain is extremely rugged.
+                </p>
+                <ButtonType2 text = 'READ THE STORY' dark = {true}/>
+            </div>
+        </div>
     )
 }
 
